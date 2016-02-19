314e143f4647ccac05fdd0df6e0bbf68064f0b9b
Pooja <pooj.kanchan@gmail.com>
2016-02-19 00:52:05 -0800

    Implemented webserver to read connection request and send hardcoded response

--- response.rb
+++ response.rb
@@ -1,2 +1,54 @@
 class Response
+
+  CONTENT_LENGTH_HEADER = "Content-Length"
+
+  def initialize
+    @version = 1.1
+    @headers = {
+                "Content-Type"      =>  "text/html",
+                "Content-Length"    =>  "0",
+                "Content-Language"  =>  "en",
+                "WWW-Authenticate"  =>  "Basic"
+               }
+
+    @body = "<html>
+               <body>
+                  This is my response
+               </body>
+             </html>"
+
+    if ! @body.empty?
+        @headers["Content-Length"] = @body.length
+    end
+    @response_code=200
+    @reason_phrase="OK"
+  end
+ 
+  def to_s
+    str = ""
+    str = str + @version.to_s
+    str = str + " "
+    str = str + @response_code.to_s
+    str = str + " "
+    str = str + @reason_phrase
+    #str = str + "\n"
+   
+    _content_length =0
+    
+    @headers.each do |key,val| 
+       str = str + "\n#{key}: #{val}"
+       if key.casecmp(CONTENT_LENGTH_HEADER) == 0
+              _content_length =val.to_i
+       end
+    end
+ 
+    if _content_length > 0
+       str = str+ "\n" + @body
+    end
+ 
+   return str
+ end
 end
+
+#test_response = Response.new
+#puts test_response.to_s
--- webserver.rb
+++ webserver.rb
@@ -1,3 +1,46 @@
-class WebServer
+require 'socket'
+require_relative 'request'
+require_relative 'response'
 
+class WebServer
+  attr_reader :options
+
+  DEFAULT_PORT = 56789
+
+  def initialize(options={})
+    @options = options
+  end
+
+  def start
+    response = ""
+    loop do
+      puts "Opening server socket to listen for connections"
+      client = server.accept
+#      client.read_nonblock(1024,response);
+       while next_line_readable?(client)
+        line = client.gets
+        puts line.chop
+      end
+
+      #code to create a new request
+      puts "Writing message"
+      test_response = Response.new
+      client.puts test_response
+
+      client.close
+    end
+  end
+
+  def server
+    @server ||= TCPServer.open(options.fetch(:port, DEFAULT_PORT))
+  end
+
+   # fd be nil if next line cannot be read
+    def next_line_readable?(socket)
+      readfds, writefds, exceptfds = select([socket], nil, nil, 0.1)
+      readfds 
+    end
 end
+
+webserver_test = WebServer.new
+webserver_test.start
