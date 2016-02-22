9433cc9a58aa972440ac9452cd6e38566688707a
adrilee <alee8@mail.sfsu.edu>
2016-02-21 21:39:00 -0800

    Combined with Tenzin's request class code.

--- request.rb
+++ request.rb
@@ -1,21 +1,5 @@
 class Request
-  attr_reader :http
-  attr_reader :headers
-  attr_reader :body
-  attr_reader :test
-  attr_reader :test2
-  attr_reader :parsed
-
-  def initialize
-    @test =<<PARAGRAPH
-GET /hello.htm HTTP/1.1
-User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
-Host: www.tutorialspoint.com
-Accept-Language: en-us
-Accept-Encoding: gzip, deflate
-Connection: Keep-Alive
-PARAGRAPH
-    @test2 =<<PARAGRAPH
+=begin EXAMPLE HTTP REQUEST USED FOR TESTING
 POST /cgi-bin/process.cgi HTTP/1.1
 User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
 Host: www.tutorialspoint.com
@@ -26,58 +10,47 @@ Accept-Encoding: gzip, deflate
 Connection: Keep-Alive
 
 licenseID=string&content=string&/paramsXML=string
-PARAGRAPH
-    @headers = Array.new
-    @body = Array.new
+=end attr_accessor :content, :http_method, :uri, :version, :headers, :body
+    # Request creation 
 
-  end
+    def initialize(request)
+      
+      @content = request.split("\n")
+      non_header_info = @content[0].split(" ")
+      @http_method = non_header_info[0]
+      @uri =  non_header_info[1]
+      @version = non_header_info[2] 
+      @headers = Array.new
+      @body = Array.new
+ 
+    end
 
-  def parsetest
-    @parsed = @test2.split("\n")
-    @http = @parsed[0]
-    index = 0
-    unparsed_headers = Array.new
-    parsed_headers = Array.new
-    1.upto(@parsed.length - 1) { |i| 
-      if(@parsed[i].length == 0)
-	puts "FOUND"
+    #Parse the request
+    def parse
+      #uri = uri.split('?')[0]
+      index = 0
+      flagged = 0
+      unparsed_headers = Array.new
+      parsed_headers = Array.new
+      1.upto(@content.length - 1) { |i| 
+      if(flagged == 1 && @content[i].length == 0)
+	#puts "FOUND"
         index = i + 1
         break
       end
-    unparsed_headers.push @parsed[i]
-    }
-    if(index != 0)
-      index.upto(@parsed.length - 1) { |i|
-        @body.push @parsed[i]
-      }
-    end
-    parsed_headers = unparsed_headers.map{|x| x.split(":")}
-    parsed_headers.each_index{ |x| parsed_headers[x][1] = parsed_headers[x][1].strip}
-    @headers = Hash[parsed_headers.map{|key, value| [key, value]}]
-  end      
-
-
-  def parse(http_request)
-    @parsed = http_request.split("\n")
-    @http = @parsed[0]
-    index = 0
-    unparsed_headers = Array.new
-    parsed_headers = Array.new
-    1.upto(@parsed.length - 1) { |i| 
-      if(@parsed[i].length == 0)
-	puts "FOUND"
-        index = i + 1
-        break
+      unparsed_headers.push @content[i]
+      if(flagged == 0 && (@content[i].include? "Content-Length") == true)
+        #puts "FOUND 2"
+        flagged = 1
       end
-    unparsed_headers.push @parsed[i]
-    }
-    if(index != 0)
-      index.upto(@parsed.length - 1) { |i|
-        @body.push @parsed[i]
       }
+      if(flagged != 0)
+        index.upto(@content.length - 1) { |i|
+          @body.push @content[i]
+        }
+      end
+      parsed_headers = unparsed_headers.map{|x| x.split(":")}
+      @headers = Hash[parsed_headers.map{|key, value| [key, value.strip]}]
+      return self
     end
-    parsed_headers = unparsed_headers.map{|x| x.split(":")}
-    parsed_headers.each_index{ |x| parsed_headers[x][1] = parsed_headers[x][1].strip}
-    @headers = Hash[parsed_headers.map{|key, value| [key, value]}]
-  end
 end
