6e9d5a9754cc7f82cc39f26a7b53a1cc64552331
Tenzin Wangchuk <Wangchukbhopa@gmail.com>
2016-03-07 17:04:47 -0800

    updated authorized

--- htaccess_checker.rb
+++ htaccess_checker.rb
@@ -1,55 +1,63 @@
 
-      require 'base64'
-      require 'digest'
+require 'base64'
+require 'digest'
 
 class HtaccessChecker
 
-  attr_reader :path,:request,:doc_root
+  attr_reader :path,:request,:doc_root, :file_path, :htpasswdcontent
 
   def initialize(path, request,document_root)
-     @path = path
-     @request = request
-     @doc_root = document_root
-  end
-  
-  def protected?
-     flag = false
-     appended_path = ""
-     @path.split(File::SEPARATOR).map do |subdir|
-         appended_path.concat(subdir=="" ? File::SEPARATOR : File::SEPARATOR + subdir)
-         puts "checking path " + @doc_root + appended_path + "/.htaccess"
-         if(File.exists?(@doc_root + appended_path + "/.htaccess")) 
-         file_path = @doc_root << appended_path << "/.htaccess"    
-            flag = true
-            break
-         end
+   @path = path
+   @request = request
+   @doc_root = document_root
+   @file_path =''
+   @content =Hash.new
+   @htpasswdcontent= Hash.new
+
+ end
+
+ def protected?
+   flag = false
+   appended_path = ""
+   @path.split(File::SEPARATOR).map do |subdir|
+     appended_path.concat(subdir=="" ? File::SEPARATOR : File::SEPARATOR + subdir)
+     puts "checking path " + @doc_root + appended_path + "/.htaccess"
+     if(File.exists?(@doc_root + appended_path + "/.htaccess")) 
+       @file_path = @doc_root << appended_path << "/.htaccess"    
+       flag = true
+       break
      end
-    flag
-  end
+   end
+   flag
+ end
 
-  def can_authorized?
-     request.headers.has_key?("Authorization")
-  end
+ def can_authorized?
+   request.headers.has_key?("Authorization")
+ end
 
-  def authorized?
-     flag = false
-     parse_file(path)
-     encryptheader = request.headers["Authorization"].split(" ")[2]
-     decryptheader = Base64.decode64(encryptstring)
-     key,value = decryptstring.split(':')
-       if htpasswd.has_key?(key)
-        if htpasswd[key] == Digest::SHA1.base64digest(value)
-    flag = true
-  end
+ def authorized?
+   flag = false
+
+   @content = parse_file(file_path)
+   encryptheader = request.headers["Authorization"].split(" ")[1]
+   decryptheader = Base64.decode64(encryptstring)
+   key,value = decryptstring.split(':')
+
+   @htpasswdcontent = htpasswd
+   if htpasswdcontent.has_key?(key)
+
+    if value == Digest::SHA1.base64digest(htpasswdcontent[key])
+      flag = true
+    end
 
   end
 end
 
 
-      def parse_file(file_path)
-     file_lines = IO.readlines(file_path)
-      file_content = Hash.new
-file_lines.each do |line|
+def parse_file(file_path)
+ file_lines = IO.readlines(file_path)
+ file_content = Hash.new
+ file_lines.each do |line|
   key, value = line.split(" ")
   file_content[key.strip] = value.strip
 end
@@ -57,22 +65,17 @@ file_content
 end
 
 
-
-
 def htpasswd
   htpasswdlist = Hash.new
-  htpasswd = IO.readlines(file_content[AuthUserFile])
+  htpasswd = IO.readlines(@content[AuthUserFile])
   htpasswd.each do |line|
-  key, value = line.split(':')
-  value.gsub(/{SHA}/,'' )
-  htpasswdlist[key.strip] = value.strip
+    key, value = line.split(':')
+    value.gsub(/{SHA}/,'' )
+    htpasswdlist[key.strip] = value.strip
+  end
+
+  htpasswdlist
 end
-
-htpasswdlist
-end
-
-
-
 
 
 
